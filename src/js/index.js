2e3561664e25f7da585a39b18174ffbcdcaf933d
zenglicai <1151009970@qq.com>
2018-09-18 22:56:36 +0800

    完善首页

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,38 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded',()=>{
+
+    //与后端交互动态生成商品列表
+    (function(){
+        
+        //获取相关元素
+        let goodslist = document.querySelector('.goodslist');
+        //创建ajax对象
+        let xhr = new XMLHttpRequest();
+        //状态值
+        let statusCode = [200,304];
+        //处理服务器返回数据
+        xhr.onload = ()=>{
+            if(statusCode.indexOf(xhr.status) >= 0){
+                let res = JSON.parse(xhr.responseText);
+                //创建ul
+                let ul = document.createElement('ul');
+                ul.innerHTML = res.map(item=>{
+                    return `<li data-guid="${item.id}">
+                                <img src="${item.imgurl}" />
+                                <h4>${item.title}</h4>
+                                <p class="price">￥ ${item.price}</p>
+                            </li>`;
+                }).join('');
+                goodslist.innerHTML = '';
+                goodslist.appendChild(ul);
+            }
+        }
+        //连接服务器
+        xhr.open('get','src/api/index.php',true);
+        //发送请求
+        xhr.send();     
+        
+    })();
+
     //轮播图
     (function(){
         //获取相关元素
@@ -45,6 +79,54 @@ document.addEventListener('DOMContentLoaded',function(){
             }
             animate(ul,{left:-imgWidth*idx});
         }
+    })();
+
+    //动态效果
+    (function(){
+        //off部分
+        let imgs1 = document.querySelectorAll('#off .off img');
+        for(let i=0;i<imgs1.length;i++){
+            imgs1[i].onmouseover = function(){
+                animate(this,{top:-8});
+            }
+            imgs1[i].onmouseleave = function(){
+                animate(this,{top:0});
+            }
+        }
+
+        //find部分
+        let imgs2 = document.querySelectorAll('#find .find img');
+        for(let i=0;i<imgs2.length;i++){
+            imgs2[i].onmouseover = function(){
+                animate(this,{left:-8});
+            }
+            imgs2[i].onmouseleave = function(){
+                animate(this,{left:0});
+            }
+        }
+
+        //middle_banner部分
+        let as = document.querySelectorAll('.middle_banner a');
+        for(let i=0;i<as.length;i++){
+            as[i].onmouseover = function(){
+                animate(this,{opacity:0.7});
+            }
+            as[i].onmouseleave = function(){
+                animate(this,{opacity:1});
+            }
+        }
+
+        //life部分
+        // let imgs3 = document.querySelectorAll('#life .life img');
+        // for(let i=0;i<imgs3.length;i++){
+        //     imgs3[i].onmouseover = function(){
+        //         animate(this,{left:-8});
+        //     }
+        //     imgs3[i].onmouseleave = function(){
+        //         animate(this,{left:-8});
+        //     }
+        // }
+        
     })();
 
 });
